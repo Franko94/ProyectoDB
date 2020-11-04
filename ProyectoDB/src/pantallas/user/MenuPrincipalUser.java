74a6966940e8f000f9caeb88972e2e3ae8f8cd4a
AgustinPicos <agustinpicos@gmail.com>
2020-11-03 21:24:14 -0300

    cambio de nombre de paquete
    el paquete pantallas.user tendría todas las cosas de la app ususario

--- ProyectoDB/src/pantallas/user/MenuPrincipalUser.java
+++ ProyectoDB/src/pantallas/user/MenuPrincipalUser.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pantallas.usuario.solicitudes.de.usuario;
+package pantallas.user;
 
 /**
  *
