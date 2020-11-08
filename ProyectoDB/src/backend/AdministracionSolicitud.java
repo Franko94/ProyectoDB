158d5270c956a5e79d250bd74ce986cebb110749
Agustin Picos <agustinpicos@gmail.com>
2020-11-08 00:28:51 -0300

    Reparado set ususario autoprizante en solicitud

--- ProyectoDB/src/backend/AdministracionSolicitud.java
+++ ProyectoDB/src/backend/AdministracionSolicitud.java
@@ -211,5 +211,6 @@ public class AdministracionSolicitud {
         stmt.setString(1, Configuracion.usuario);
         stmt.setInt(2, Integer.parseInt(id_solicitud));
         stmt.executeUpdate();
+        System.out.println(stmt.toString());
     }
 }
